8cf0d522cd2bb8125104e47ab02d9ee1986e99e4
s-akibayashi <121149780+s-akibayashi@users.noreply.github.com>
2022-12-21 15:10:29 +0900

    Update ec2-ss-ami.sh

--- ec2-ss-ami.sh
+++ ec2-ss-ami.sh
@@ -9,10 +9,7 @@ read TICKET
 Region=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e 's/.$//')
 InstanceId=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 MyInstance=$(hostname)
-Target_Date=$(date "+%Y-%m-%dT%H:%M" -u -d '-1 hours')
-Delete_Date=$(date "+%Y-%m-%dT%H:%M" -u -d '-1 day')
-Now_Date=$(date "+%Y-%m-%dT%H:%M" -u)
-Tag_Date=$(date "+%Y-%m-%d-%H-%M")
+HOST_AND_DATE="$(hostname)_$(date '+%Y%m%d')"
 
 export AWS_DEFAULT_REGION=${Region}
 
@@ -26,15 +23,20 @@ EC2_VolumeId=$( \
 EC2_VolumeIds=(`echo $EC2_VolumeId`)
 
 snapshot() {
-  ## スナップショットの作成
-  Snapshot=$(aws ec2 describe-snapshots \
-    --filters "Values=$InstanceId" \
-    --query "Snapshots[?(StartTime>='$Target_Date') && (StartTime<='$Now_Date')].[SnapshotId,StartTime]" \
-    --tag-specifications 'ResourceType=snapshot,Tags=[{Key=Name,Value='$HOST_AND_DATE'},{Key=TICKET,Value='$TICKET'}]' 
+## スナップショットの作成 
+Snapshot=$(aws ec2 describe-snapshots \
+  --filters "Name=tag:Auto_SS_TG,Values=$InstanceId" \
+  --region ap-northeast-1 \
+  --output text | head -n1)
+
+if [ -z "$Snapshot" ]; then
+    for VolumeId in ${EC2_VolumeIds[@]}; do
+      aws ec2 create-snapshot --volume-id ${VolumeId} --tag-specifications 'ResourceType=snapshot,Tags=[{Key=Name,Value='$HOST_AND_DATE'},{Key=TICKET,Value='$TICKET'}]'  --description "created by $0"
+    done
+fi
 }
 
 ami() {
-  HOST_AND_DATE="$(hostname)_$(date '+%Y%m%d')"
   aws ec2 create-image \
       --instance-id $(curl -s http://169.254.169.254/latest/meta-data/instance-id) \
       --name ${HOST_AND_DATE} \
